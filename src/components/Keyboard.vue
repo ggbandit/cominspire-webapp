bcb879c525c0f98c0caf2504a221571d2c2e86bd
ggbandit <ggbandit@hotmail.com>
2018-10-06 20:43:46 +0700

    mouse

--- src/components/Keyboard.vue
+++ src/components/Keyboard.vue
@@ -2,61 +2,155 @@
 <div class="bg">
     <Navbar />
     <div class="topic mx-auto">
-      <p class="text m-auto">Top 5 Keyboards 2018</p>
+      <p class="text m-auto">Top 5 Notebooks 2018</p>
     </div>
     <div class="card mx-auto mt-4 px-5 py-4">
       <div class="row height-50">
-        <div class="col-4" style="background-color: white; border: 2px solid #ccc">image1</div>
-        <div class="col-4" style="background-color: white; border: 2px solid #ccc">name1</div>
-        <div class="col-4" style="background-color: white; border: 2px solid #ccc">logo1</div>
+        <div class="col-4" ><img style="width: 100%;height: 100%;" src="../assets/msi.jpg"></div>
+        <div class="col-4 text-name">MSI GF63 8RC</div>
+        <div class="col-4" ><img class="com-logo" src="../assets/intel.png"><img class="com-logo" src="../assets/nvidia.png"></div>
       </div>
       <div class="row height-50">
-        <div class="col-6" style="background-color: white; border: 2px solid #ccc">detail1</div>
-        <div class="col-6" style="background-color: white; border: 2px solid #ccc">price1</div>
-      </div>
-    </div>
-    <div class="card mx-auto mt-4 px-5 py-4">
-      <div class="row height-50">
-        <div class="col-4" style="background-color: white; border: 2px solid #ccc">image2</div>
-        <div class="col-4" style="background-color: white; border: 2px solid #ccc">name2</div>
-        <div class="col-4" style="background-color: white; border: 2px solid #ccc">logo2</div>
-      </div>
-      <div class="row height-50">
-        <div class="col-6" style="background-color: white; border: 2px solid #ccc">detail2</div>
-        <div class="col-6" style="background-color: white; border: 2px solid #ccc">price2</div>
-      </div>
-    </div>
-    <div class="card mx-auto mt-4 px-5 py-4">
-      <div class="row height-50">
-        <div class="col-4" style="background-color: white; border: 2px solid #ccc">image3</div>
-        <div class="col-4" style="background-color: white; border: 2px solid #ccc">name3</div>
-        <div class="col-4" style="background-color: white; border: 2px solid #ccc">logo3</div>
-      </div>
-      <div class="row height-50">
-        <div class="col-6" style="background-color: white; border: 2px solid #ccc">detail3</div>
-        <div class="col-6" style="background-color: white; border: 2px solid #ccc">price3</div>
+        <div class="col-6 detail" >
+          <p>- Intel Core i7-8750H (2.20 - 4.10 GHz) </p>
+          <p>- NVIDIA GeForce GTX 1050 (4GB GDDR5)</p>
+          <p>- 8 GB DDR4</p>
+          <p>- 1 TB 5400 RPM + 128 GB SSD</p>
+          <p>- 15.6 inch (1920x1080) Full HD IPS</p>
+          <p>- Windows 10 Home (64 Bit)</p>
+        </div>
+        <div class="col-6 border" >
+          <div class="row no-gutters">
+            <div class="col">
+              <div class="row no-gutters"><img class="sell-logo" src="../assets/jib.jpg"></div>
+              <div class="row no-gutters"><img class="sell-logo mt-3" src="../assets/advice.png"></div>
+            </div>
+            <div class="col">
+              <div class="row no-gutters"><p class="text-color mt-3">ราคา 29,900 บาท</p></div>
+              <div class="row no-gutters"><p class="text-color mt-5">ราคา 29,900 บาท</p></div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
     <div class="card mx-auto mt-4 px-5 py-4">
       <div class="row height-50">
-        <div class="col-4" style="background-color: white; border: 2px solid #ccc">image4</div>
-        <div class="col-4" style="background-color: white; border: 2px solid #ccc">name4</div>
-        <div class="col-4" style="background-color: white; border: 2px solid #ccc">logo4</div>
+        <div class="col-4" ><img style="width: 100%;height: 100%;" src="../assets/acer.jpg"></div>
+        <div class="col-4 text-name">Acer Nitro 5 AN515-52</div>
+        <div class="col-4" ><img class="com-logo" src="../assets/intel.png"><img class="com-logo" src="../assets/nvidia.png"></div>
       </div>
       <div class="row height-50">
-        <div class="col-6" style="background-color: white; border: 2px solid #ccc">detail4</div>
-        <div class="col-6" style="background-color: white; border: 2px solid #ccc">price4</div>
+        <div class="col-6 detail" >
+          <p>- Intel Core i5-8300H (2.30 - 4.00 GHz) </p>
+          <p>- NVIDIA GeForce GTX 1050Ti (4GB GDDR5)</p>
+          <p>- 8 GB DDR4</p>
+          <p>- 1 TB 5400 RPM</p>
+          <p>- 15.6 inch (1920x1080) Full HD IPS</p>
+          <p>- Windows 10 Home (64 Bit)</p>
+
+        </div>
+        <div class="col-6 border" >
+          <div class="row no-gutters">
+            <div class="col">
+              <div class="row no-gutters"><img class="sell-logo" src="../assets/jib.jpg"></div>
+              <div class="row no-gutters"><img class="sell-logo mt-3" src="../assets/advice.png"></div>
+            </div>
+            <div class="col">
+              <div class="row no-gutters"><p class="text-color mt-3">ราคา 25,900 บาท</p></div>
+              <div class="row no-gutters"><p class="text-color mt-5">ราคา 25,900 บาท</p></div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
     <div class="card mx-auto mt-4 px-5 py-4">
       <div class="row height-50">
-        <div class="col-4" style="background-color: white; border: 2px solid #ccc">image5</div>
-        <div class="col-4" style="background-color: white; border: 2px solid #ccc">name5</div>
-        <div class="col-4" style="background-color: white; border: 2px solid #ccc">logo5</div>
+        <div class="col-4" ><img style="width: 100%;height: 100%;" src="../assets/asus.jpg"></div>
+        <div class="col-4 text-name">Asus TUF Gaming FX504GM</div>
+        <div class="col-4" ><img class="com-logo" src="../assets/intel.png"><img class="com-logo" src="../assets/nvidia.png"></div>
       </div>
       <div class="row height-50">
-        <div class="col-6" style="background-color: white; border: 2px solid #ccc">detail5</div>
-        <div class="col-6" style="background-color: white; border: 2px solid #ccc">price5</div>
+        <div class="col-6 detail" >
+          <p>- Intel Core i7-8750H (2.20 - 4.10 GHz) </p>
+          <p>- NVIDIA GeForce GTX 1060 (6GB GDDR5)</p>
+          <p>- 8 GB DDR4</p>
+          <p>- 1 TB 5400 RPM + 128 GB SSD PCIe M.2</p>
+          <p>- 15.6 inch (1920x1080) Full HD 120Hz</p>
+          <p>- Windows 10 Home (64 Bit)</p>
+
+        </div>
+        <div class="col-6 border" >
+          <div class="row no-gutters">
+            <div class="col">
+              <div class="row no-gutters"><img class="sell-logo" src="../assets/jib.jpg"></div>
+              <div class="row no-gutters"><img class="sell-logo mt-3" src="../assets/advice.png"></div>
+            </div>
+            <div class="col">
+              <div class="row no-gutters"><p class="text-color mt-3">ราคา 39,500 บาท</p></div>
+              <div class="row no-gutters"><p class="text-color mt-5">ราคา 39,500 บาท</p></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="card mx-auto mt-4 px-5 py-4">
+      <div class="row height-50">
+        <div class="col-4" ><img style="width: 100%;height: 100%;" src="../assets/dell.jpg"></div>
+        <div class="col-4 text-name">DELL G3 15 3579 Gaming</div>
+        <div class="col-4" ><img class="com-logo" src="../assets/intel.png"><img class="com-logo" src="../assets/nvidia.png"></div>
+      </div>
+      <div class="row height-50">
+        <div class="col-6 detail" >
+          <p>- Intel Core i5-8300H (2.30 - 4.00 GHz) </p>
+          <p>- NVIDIA GeForce GTX 1050 (4GB GDDR5)</p>
+          <p>- 4 GB DDR4</p>
+          <p>- 1 TB 5400 RPM</p>
+          <p>- 15.6 inch (1920x1080) Full HD IPS</p>
+          <p>- Ubuntu</p>
+
+        </div>
+        <div class="col-6 border" >
+          <div class="row no-gutters">
+            <div class="col">
+              <div class="row no-gutters"><img class="sell-logo" src="../assets/jib.jpg"></div>
+              <div class="row no-gutters"><img class="sell-logo mt-3" src="../assets/advice.png"></div>
+            </div>
+            <div class="col">
+              <div class="row no-gutters"><p class="text-color mt-3">ราคา 23,490 บาท</p></div>
+              <div class="row no-gutters"><p class="text-color mt-5">ราคา 23,490 บาท</p></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="card mx-auto mt-4 px-5 py-4">
+      <div class="row height-50">
+        <div class="col-4" ><img style="width: 100%;height: 100%;" src="../assets/lenovo.jpg"></div>
+        <div class="col-4 text-name">Lenovo Legion Y520</div>
+        <div class="col-4" ><img class="com-logo" src="../assets/intel.png"><img class="com-logo" src="../assets/nvidia.png"></div>
+      </div>
+      <div class="row height-50">
+        <div class="col-6 detail" >
+          <p>- Intel Core i7-7700HQ (2.80 - 3.80 GHz) </p>
+          <p>- NVIDIA GeForce GTX 1050 (4GB GDDR5)</p>
+          <p>- 4 GB DDR4</p>
+          <p>- 1 TB 5400 RPM</p>
+          <p>- 15.6 inch (1920x1080) Full HD IPS</p>
+          <p>- DOS Operating System</p>
+
+        </div>
+        <div class="col-6 border" >
+          <div class="row no-gutters">
+            <div class="col">
+              <div class="row no-gutters"><img class="sell-logo" src="../assets/jib.jpg"></div>
+              <div class="row no-gutters"><img class="sell-logo mt-3" src="../assets/advice.png"></div>
+            </div>
+            <div class="col">
+              <div class="row no-gutters"><p class="text-color mt-3">ราคา 25,900 บาท</p></div>
+              <div class="row no-gutters"><p class="text-color mt-5">ราคา 26,900 บาท</p></div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
 </div>
@@ -92,6 +186,12 @@ export default {
   width: 1167px;
   height: 124px;
 }
+.detail {
+  color: #FFFFFF;
+  font-family: Lato;
+  font-size: 18px;
+  text-align: left;
+}
 .text {
   color: #47525E;
   font-family: Lato;
@@ -102,6 +202,19 @@ export default {
   text-align: center;
   padding-top: 60px;
 }
+.text-name {
+  color: white;
+  padding-top: 55px;
+  font-size: 30px;
+}
+.text-color {
+  color: #FC0606;
+  font-family: Lato;
+  font-size: 30px;
+  font-weight: 700;
+  line-height: 39px;
+  width: 232px;
+}
 .card {
   background-color: #343F4B;
   border-radius: 20px;
@@ -110,5 +223,22 @@ export default {
 }
 .height-50 {
   height: 50vh;
+}
+.com-logo {
+  margin-top: 40px;
+  margin-left: 15px;
+  width: 150px;
+  height: 150px;
+}
+.border {
+  background-color: rgba(19, 206, 102, 0);
+  border: 7px solid #FFFFFF;
+  width: 500px;
+  height: 216px;
+  padding: 10px;
+}
+.sell-logo {
+  width: 131px;
+  height: 84px;
 }
 </style>
--- src/components/Mouse.vue
+++ src/components/Mouse.vue
@@ -2,61 +2,152 @@
 <div class="bg">
     <Navbar />
     <div class="topic mx-auto">
-      <p class="text m-auto">Top 5 Mouses 2018</p>
+      <p class="text m-auto">Top 5 Notebooks 2018</p>
     </div>
     <div class="card mx-auto mt-4 px-5 py-4">
       <div class="row height-50">
-        <div class="col-4" style="background-color: white; border: 2px solid #ccc">image1</div>
-        <div class="col-4" style="background-color: white; border: 2px solid #ccc">name1</div>
-        <div class="col-4" style="background-color: white; border: 2px solid #ccc">logo1</div>
+        <div class="col-4" ><img style="width: 100%;height: 100%;" src="../assets/basilisk-essential_1.jpg"></div>
+        <div class="col-4 text-name">RAZER Basilisk Essential</div>
+        <div class="col-4" ><img class="com-logo" src="../assets/razer.png"></div>
       </div>
       <div class="row height-50">
-        <div class="col-6" style="background-color: white; border: 2px solid #ccc">detail1</div>
-        <div class="col-6" style="background-color: white; border: 2px solid #ccc">price1</div>
-      </div>
-    </div>
-    <div class="card mx-auto mt-4 px-5 py-4">
-      <div class="row height-50">
-        <div class="col-4" style="background-color: white; border: 2px solid #ccc">image2</div>
-        <div class="col-4" style="background-color: white; border: 2px solid #ccc">name2</div>
-        <div class="col-4" style="background-color: white; border: 2px solid #ccc">logo2</div>
-      </div>
-      <div class="row height-50">
-        <div class="col-6" style="background-color: white; border: 2px solid #ccc">detail2</div>
-        <div class="col-6" style="background-color: white; border: 2px solid #ccc">price2</div>
-      </div>
-    </div>
-    <div class="card mx-auto mt-4 px-5 py-4">
-      <div class="row height-50">
-        <div class="col-4" style="background-color: white; border: 2px solid #ccc">image3</div>
-        <div class="col-4" style="background-color: white; border: 2px solid #ccc">name3</div>
-        <div class="col-4" style="background-color: white; border: 2px solid #ccc">logo3</div>
-      </div>
-      <div class="row height-50">
-        <div class="col-6" style="background-color: white; border: 2px solid #ccc">detail3</div>
-        <div class="col-6" style="background-color: white; border: 2px solid #ccc">price3</div>
+        <div class="col-6 detail" >
+          <p>- Interface : USB</p>
+          <p>- Led : RGB</p>
+          <p>- Max DPI : 6400</p>
+          <p>- Movement Technology : Optical</p>
+          <p>- Sensor : 4G Optical Sensor</p>
+        </div>
+        <div class="col-6 border" >
+          <div class="row no-gutters">
+            <div class="col">
+              <div class="row no-gutters"><img class="sell-logo" src="../assets/jib.jpg"></div>
+              <div class="row no-gutters"><img class="sell-logo mt-3" src="../assets/advice.png"></div>
+            </div>
+            <div class="col">
+              <div class="row no-gutters"><p class="text-color mt-3">ราคา 1,590 บาท</p></div>
+              <div class="row no-gutters"><p class="text-color mt-5">ราคา 1,990 บาท</p></div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
     <div class="card mx-auto mt-4 px-5 py-4">
       <div class="row height-50">
-        <div class="col-4" style="background-color: white; border: 2px solid #ccc">image4</div>
-        <div class="col-4" style="background-color: white; border: 2px solid #ccc">name4</div>
-        <div class="col-4" style="background-color: white; border: 2px solid #ccc">logo4</div>
+        <div class="col-4" ><img style="width: 100%;height: 100%;" src="../assets/mouselogotech1.jpg"></div>
+        <div class="col-4 text-name">LOGITECH G304 Lightspeed</div>
+        <div class="col-4" ><img class="com-logo" src="../assets/logotech.png"></div>
       </div>
       <div class="row height-50">
-        <div class="col-6" style="background-color: white; border: 2px solid #ccc">detail4</div>
-        <div class="col-6" style="background-color: white; border: 2px solid #ccc">price4</div>
+        <div class="col-6 detail" >
+          <p>- Interface : USB + Wireless</p>
+          <p>- Led : -</p>
+          <p>- Max DPI : 12000</p>
+          <p>- Movement Technology : Optical</p>
+          <p>- Sensor : Logitech HERO sensor</p>
+
+
+        </div>
+        <div class="col-6 border" >
+          <div class="row no-gutters">
+            <div class="col">
+              <div class="row no-gutters"><img class="sell-logo" src="../assets/advice.png"></div>
+              <div class="row no-gutters"></div>
+            </div>
+            <div class="col">
+              <div class="row no-gutters"><p class="text-color mt-3">ราคา 1,290 บาท</p></div>
+              <div class="row no-gutters"><p class="text-color mt-5"></p></div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
     <div class="card mx-auto mt-4 px-5 py-4">
       <div class="row height-50">
-        <div class="col-4" style="background-color: white; border: 2px solid #ccc">image5</div>
-        <div class="col-4" style="background-color: white; border: 2px solid #ccc">name5</div>
-        <div class="col-4" style="background-color: white; border: 2px solid #ccc">logo5</div>
+        <div class="col-4" ><img style="width: 100%;height: 100%;" src="../assets/ASUS ROG Strix Evolve.jpg"></div>
+        <div class="col-4 text-name">ASUS ROG Strix Evolve</div>
+        <div class="col-4" ><img class="com-logo" src="../assets/asus.png"></div>
       </div>
       <div class="row height-50">
-        <div class="col-6" style="background-color: white; border: 2px solid #ccc">detail5</div>
-        <div class="col-6" style="background-color: white; border: 2px solid #ccc">price5</div>
+        <div class="col-6 detail" >
+          <p>- Interface : USB</p>
+          <p>- Led : RGB</p>
+          <p>- Max DPI : 7200</p>
+          <p>- Movement Technology : Optical</p>
+          <p>- Sensor : PixArt PMW3330</p>
+
+
+        </div>
+        <div class="col-6 border" >
+          <div class="row no-gutters">
+            <div class="col">
+              <div class="row no-gutters"><img class="sell-logo" src="../assets/advice.png"></div>
+              <div class="row no-gutters"></div>
+            </div>
+            <div class="col">
+              <div class="row no-gutters"><p class="text-color mt-3">ราคา 1,590 บาท</p></div>
+              <div class="row no-gutters"><p class="text-color mt-5"></p></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="card mx-auto mt-4 px-5 py-4">
+      <div class="row height-50">
+        <div class="col-4" ><img style="width: 100%;height: 100%;" src="../assets/razermouse.jpg"></div>
+        <div class="col-4 text-name">RAZER DeathAdder</div>
+        <div class="col-4" ><img class="com-logo" src="../assets/razer.png"></div>
+      </div>
+      <div class="row height-50">
+        <div class="col-6 detail" >
+          <p>- Interface : USB 2.0</p>
+          <p>- Led : Green</p>
+          <p>- Max DPI : 6400</p>
+          <p>- Movement Technology : Optical</p>
+          <p>- Sensor : 4G Optical Sensor</p>
+
+        </div>
+        <div class="col-6 border" >
+          <div class="row no-gutters">
+            <div class="col">
+              <div class="row no-gutters"><img class="sell-logo" src="../assets/jib.jpg"></div>
+              <div class="row no-gutters"></div>
+            </div>
+            <div class="col">
+              <div class="row no-gutters"><p class="text-color mt-3">ราคา 1,590 บาท</p></div>
+              <div class="row no-gutters"><p class="text-color mt-5"></p></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="card mx-auto mt-4 px-5 py-4">
+      <div class="row height-50">
+        <div class="col-4" ><img style="width: 100%;height: 100%;" src="../assets/asusmouse.jpg"></div>
+        <div class="col-4 text-name">ASUS Cerberus Fortus</div>
+        <div class="col-4" ><img class="com-logo" src="../assets/asus.png"></div>
+      </div>
+      <div class="row height-50">
+        <div class="col-6 detail" >
+          <p>- Interface : USB</p>
+          <p>- Led : RGB</p>
+          <p>- Max DPI : 4000</p>
+          <p>- Movement Technology : Optical</p>
+          <p>- Sensor : Avago 5050</p>
+
+        </div>
+        <div class="col-6 border" >
+          <div class="row no-gutters">
+            <div class="col">
+              <div class="row no-gutters"><img class="sell-logo" src="../assets/advice.png"></div>
+              <div class="row no-gutters"></div>
+            </div>
+            <div class="col">
+              <div class="row no-gutters"><p class="text-color mt-3">ราคา 1,490 บาท</p></div>
+              <div class="row no-gutters"><p class="text-color mt-5"></p></div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
 </div>
@@ -92,6 +183,12 @@ export default {
   width: 1167px;
   height: 124px;
 }
+.detail {
+  color: #FFFFFF;
+  font-family: Lato;
+  font-size: 18px;
+  text-align: left;
+}
 .text {
   color: #47525E;
   font-family: Lato;
@@ -102,6 +199,19 @@ export default {
   text-align: center;
   padding-top: 60px;
 }
+.text-name {
+  color: white;
+  padding-top: 55px;
+  font-size: 30px;
+}
+.text-color {
+  color: #FC0606;
+  font-family: Lato;
+  font-size: 30px;
+  font-weight: 700;
+  line-height: 39px;
+  width: 232px;
+}
 .card {
   background-color: #343F4B;
   border-radius: 20px;
@@ -110,5 +220,22 @@ export default {
 }
 .height-50 {
   height: 50vh;
+}
+.com-logo {
+  margin-top: 40px;
+  margin-left: 15px;
+  width: 150px;
+  height: 150px;
+}
+.border {
+  background-color: rgba(19, 206, 102, 0);
+  border: 7px solid #FFFFFF;
+  width: 500px;
+  height: 216px;
+  padding: 10px;
+}
+.sell-logo {
+  width: 131px;
+  height: 84px;
 }
 </style>
